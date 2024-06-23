e31da08f573dadf9f3cd5306ef9a4b4bbb6c51e7
tmnaeem <tnaeemtrashid@email.com>
2024-06-23 16:45:56 +0800

    temp

--- js/card-main.js
+++ js/card-main.js
@@ -1,8 +1,16 @@
 $(function(){
    $('body').on('click', '#btn-open', function(){
-
-    $('body').find('.start-overlay').addClass('animated');
-    $('body').find('.start-overlay').addClass('slideOutUp');
+    
+    $('body').find('.logo-ns').addClass('animated');
+    $('body').find('.logo-ns').addClass('fadeOut');
+    $('body').find('.btn').addClass('animated');
+    $('body').find('.btn').addClass('fadeOut');
+    
+    
+    setTimeout(() => {
+        $('body').find('.start-overlay').addClass('animated');
+        $('body').find('.start-overlay').addClass('slideOutUp');
+    }, 450);
 
     setTimeout(() => {
         $('body').find('.opening-card').addClass('d-none');
@@ -10,6 +18,6 @@ $(function(){
         $('body').find('.navbar-card').removeClass('d-none');
         $('body').find('#music-btn').trigger('click');
         AOS.refresh();  // ---  need to reinitiate after unhide
-    }, 750);
+    }, 950);
    })
 })
