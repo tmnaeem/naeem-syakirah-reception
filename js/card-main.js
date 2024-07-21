51871e6bfb8509baf2b0f522f954667f4e91f366
tmnaeem <tnaeemtrashid@email.com>
2024-07-22 02:57:55 +0800

    for syakirah's first iteration

--- js/card-main.js
+++ js/card-main.js
@@ -1,15 +1,18 @@
 $(function(){
    $('body').on('click', '#btn-open', function(){
-
-    $('body').find('.start-overlay').addClass('animated');
-    $('body').find('.start-overlay').addClass('slideOutUp');
+    
+    $('body').find('.opening-card').removeClass('d-none');
+    $('body').find('.opening-card').addClass('animated');
+    $('body').find('.opening-card').addClass('slideOutUp');
+    
 
     setTimeout(() => {
+        // $('body').find('.hidden-card').addClass('d-none');
         $('body').find('.opening-card').addClass('d-none');
         $('body').find('.main-div').removeClass('d-none');
         $('body').find('.navbar-card').removeClass('d-none');
         $('body').find('#music-btn').trigger('click');
         AOS.refresh();  // ---  need to reinitiate after unhide
-    }, 750);
+    }, 940);
    })
 })
