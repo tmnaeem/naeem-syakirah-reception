97b959026ab52276524282fcb821016d18df8993
tmnaeem <tnaeemtrashid@email.com>
2024-07-30 23:05:28 +0800

    big changes

--- js/card-main.js
+++ js/card-main.js
@@ -1,23 +1,17 @@
 $(function(){
-   $('body').on('click', '#btn-open', function(){
-    
-    $('body').find('.logo-ns').addClass('animated');
-    $('body').find('.logo-ns').addClass('fadeOut');
-    $('body').find('.btn').addClass('animated');
-    $('body').find('.btn').addClass('fadeOut');
-    
-    
-    setTimeout(() => {
-        $('body').find('.start-overlay').addClass('animated');
-        $('body').find('.start-overlay').addClass('slideOutUp');
-    }, 450);
-
-    setTimeout(() => {
-        $('body').find('.opening-card').addClass('d-none');
-        $('body').find('.main-div').removeClass('d-none');
-        $('body').find('.navbar-card').removeClass('d-none');
-        $('body').find('#music-btn').trigger('click');
-        AOS.refresh();  // ---  need to reinitiate after unhide
-    }, 950);
-   })
-})
+    $('body').on('click', '#btn-open', function(){
+     
+     $('body').find('.opening-card').removeClass('d-none');
+     $('body').find('.opening-card').addClass('animated');
+     $('body').find('.opening-card').addClass('slideOutUp');
+     
+ 
+     setTimeout(() => {
+         $('body').find('.opening-card').addClass('d-none');
+         $('body').find('.main-div').removeClass('d-none');
+         $('body').find('.navbar-card').removeClass('d-none');
+         $('body').find('#music-btn').trigger('click');
+         AOS.refresh();  // ---  need to reinitiate after unhide
+     }, 940);
+    })
+ })
